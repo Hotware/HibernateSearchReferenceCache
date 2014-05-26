aabf2e8185ed5b8a55f75e0432aa17d9def4fe06
Martin Braun <martinbraun123@aol.com>
2014-05-26 14:51:22 +0200

    fixed the annotation

--- src/main/java/de/hotware/hibernate/search/extension/reference/annotations/ReferenceCached.java
+++ src/main/java/de/hotware/hibernate/search/extension/reference/annotations/ReferenceCached.java
@@ -1,7 +1,14 @@
 package de.hotware.hibernate.search.extension.reference.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface ReferenceCached {
-	
+
 	int cacheSize();
 
 }
